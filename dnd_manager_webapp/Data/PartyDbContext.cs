0be8d00f8d77e163a5ddc8e8de89d2f35a24a300
Nightchild99 <dmnks.peter@gmail.com>
2023-12-21 13:56:05 +0100

    dbcontext added

---
+++ dnd_manager_webapp/Data/PartyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using dnd_manager_webapp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dnd_manager_webapp.Data
+{
+    public class PartyDbContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+        public PartyDbContext(DbContextOptions<PartyDbContext> opt)
+            :base(opt)
+        {
+                
+        }
+    }
+}
