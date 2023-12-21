3e53a300bce0ef66ad59188fbc224a50c14af708
Nightchild99 <dmnks.peter@gmail.com>
2023-12-21 14:10:27 +0100

    party repository added

---
+++ dnd_manager_webapp/Data/IPartyRepository.cs
@@ -0,0 +1,14 @@
+﻿using dnd_manager_webapp.Models;
+
+namespace dnd_manager_webapp.Data
+{
+    public interface IPartyRepository
+    {
+        void Create(Character character);
+        void Delete(string name);
+        IEnumerable<Character> Read();
+        Character? Read(string name);
+        Character? ReadFromId(string id);
+        void Update(Character character);
+    }
+}
---
+++ dnd_manager_webapp/Data/PartyRepository.cs
@@ -0,0 +1,55 @@
+﻿using dnd_manager_webapp.Models;
+using System.Diagnostics;
+using System.Security.Claims;
+using System.Xml.Linq;
+
+namespace dnd_manager_webapp.Data
+{
+    public class PartyRepository : IPartyRepository
+    {
+        PartyDbContext context;
+
+        public PartyRepository(PartyDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(Character character)
+        {
+            context.Characters.Add(character);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<Character> Read()
+        {
+            return context.Characters;
+        }
+
+        public Character? Read(string name)
+        {
+            return context.Characters.FirstOrDefault(t => t.Name == name);
+        }
+
+        public Character? ReadFromId(string id)
+        {
+            return context.Characters.FirstOrDefault(t => t.Id == id);
+        }
+
+        public void Update(Character character)
+        {
+            var old = Read(character.Name);
+            old.Name = character.Name;
+            old.Level = character.Level;
+            old.Race = character.Race;
+            old.Class = character.Class;
+            old.Description = $"{character.Name} is a level {character.Level} {character.Race} {character.Class}.";
+        }
+
+        public void Delete(string name)
+        {
+            var character = Read(name);
+            context.Characters.Remove(character);
+            context.SaveChanges();
+        }
+    }
+}
